a6c565298f1eb15cf75176a07ea136c84379679a
David Snopek <dsnopek@gmail.com>
2021-11-23 21:06:14 -0600

    Test with a couple more STUN servers

--- Main.gd
+++ Main.gd
@@ -7,6 +7,14 @@ var stun_client: StunClient
 func _ready() -> void:
 	stun_client = StunClient.new('69.164.203.66', 443)
 	#stun_client = StunClient.new('2600:3c00::f03c:92ff:fe8c:017a', 443)
+	
+	# stun.l.google.com
+	#stun_client = StunClient.new('173.194.196.127', 19302)
+	#stun_client = StunClient.new('2607:f8b0:4001:c0f::7f', 19302)
+	
+	# global.stun.twilio.com (no IPv6?)
+	#stun_client = StunClient.new('34.203.250.120', 3478)
+	
 	stun_client.connect("message_received", self, "_on_stun_client_message_received")
 	stun_client.send_binding_request()
 
